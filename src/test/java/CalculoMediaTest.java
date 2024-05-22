0b13693935f5d263f4d479e7b23d3d00e16f16bf
Renef13 <renefricardo@gmail.com>
2024-05-22 18:25:22 -0300

    Adição do ultimo Teste.

--- src/test/java/CalculoMediaTest.java
+++ src/test/java/CalculoMediaTest.java
@@ -5,6 +5,7 @@ import model.TurmaModel;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculoMediaTest {
 
@@ -84,6 +85,26 @@ public class CalculoMediaTest {
 
         assertEquals(10, renef.getMedia(), "A média deve ser 10.0.");
     }
+    @Test
+    public void testLimiteDeSala() {
+        AlunoController alunoController = new AlunoController();
+        TurmaController turmaController = new TurmaController();
 
+        turmaController.criarTurma("Qualidade de Software");
+
+        for (int i = 0; i < 5; i++) {
+            alunoController.criarAluno("Aluno " + (i + 1));
+            AlunoModel aluno = alunoController.buscarAlunos("Aluno " + (i + 1)).getFirst();
+            turmaController.adicionarAlunoNaTurma("Qualidade de Software", aluno);
+        }
+
+        alunoController.criarAluno("Aluno 6");
+        AlunoModel aluno6 = alunoController.buscarAlunos("Aluno 6").getFirst();
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            turmaController.adicionarAlunoNaTurma("Qualidade de Software", aluno6);
+        });
+        assertEquals("A sala não pode ter mais de 5 discentes.", exception.getMessage());
+    }
 
 }
